ecd2b882f31d93c14684f2fa5bba8f4ffdb108ab
Bilal Ajaha <bilalajaha@gmail.com>
2024-03-23 23:29:04 +0000

    upd

---
+++ models/state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+import os
+
+
+class State(BaseModel, Base):
+    """ State class """
+    __tablename__ = 'states'
+    # name = ""
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
+                          cascade="all, delete-orphan")
+
+    if os.environ['HBNB_ENV'] != 'db':
+        @property
+        def cities(self):
+            from models import storage
+            dic = storage.all('City')
+            return [].append(v for k, v in dic.items() if self.id == v['state_id'])
